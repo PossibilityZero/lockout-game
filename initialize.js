60c8e265b55e610b7f86b2e9eea294a03e96256c
Kai Nakazawa <possibilityzero@gmail.com>
2018-09-20 14:25:08 +0900

    Make board generation dynamic

--- initialize.js
+++ initialize.js
@@ -1,12 +1,28 @@
 const board = document.querySelector('.game-board');
 
-for (let i = 0; i < 20; i++) {
-    for (let j = 0; j < 20; j++) {
-        let newCell = document.createElement('div');
-        newCell.classList.add('board-cell');
-        newCell.dataset.xCoord = j;
-        newCell.dataset.yCoord = i;
-        newCell.setAttribute('id', `(${j},${i})`)
-        board.appendChild(newCell);
+function generateCells(columns, rows) {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            let newCell = document.createElement('div');
+            newCell.classList.add('board-cell');
+            newCell.dataset.xCoord = j;
+            newCell.dataset.yCoord = i;
+            newCell.setAttribute('id', `(${j},${i})`)
+            board.appendChild(newCell);
+        }
     }
 }
+
+function setBoardStyle(columns, rows, cellSize) {
+    board.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${rows}, 1fr)`
+    board.style.width = cellSize * columns + 'px';
+    board.style.height = cellSize * rows + 'px';
+}
+
+function createBoard(columns, rows, cellSize=20) {
+    generateCells(columns, rows);
+    setBoardStyle(columns, rows, cellSize);
+}
+
+createBoard(30, 20);
