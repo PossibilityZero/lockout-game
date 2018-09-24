7359c7a654c7795e53621e2526305701b1911409
Kai Nakazawa <possibilityzero@gmail.com>
2018-09-24 23:20:16 +0900

    Add interfaces for Board and Level classes

--- script.js
+++ script.js
@@ -1,47 +1,61 @@
-// Create Board
-const board = document.querySelector('.game-board');
-
-function generateCells(columns, rows) {
-    // generate a perimeter of dummy cells, to simulate an impassible boundary
-    for (let i = -1; i <= rows; i++) {
-        for (let j = -1; j <= columns; j++) {
-            let newCell = document.createElement('div');
-            newCell.classList.add('board-cell');
-            newCell.setAttribute('id', `x${j}y${i}`)
-            if (j < 0 || j >= columns || i < 0 || i >= rows) {
-                newCell.classList.add('dummy-cell');
-            } else if (j <=1 || j >= columns - 2 || i <= 1 || i >= rows - 2) {
-                newCell.classList.add('claimed-cell');
-            } else {
-                newCell.classList.add('unclaimed-cell');
+function Board(columns, rows) {
+    this.boardContainer = document.querySelector('.game-board');
+    this.toCoordsString = function(coords) {
+        return `x${coords.x}y${coords.y}`
+    };
+    this.generateCells = function(columns, rows) {
+        // generate a perimeter of dummy cells, to simulate an impassible boundary
+        for (let i = -1; i <= rows; i++) {
+            for (let j = -1; j <= columns; j++) {
+                let newCell = document.createElement('div');
+                newCell.classList.add('board-cell');
+                newCell.setAttribute('id', `x${j}y${i}`)
+                if (j < 0 || j >= columns || i < 0 || i >= rows) {
+                    newCell.classList.add('dummy-cell');
+                } else if (j <=1 || j >= columns - 2 || i <= 1 || i >= rows - 2) {
+                    newCell.classList.add('claimed-cell');
+                } else {
+                    newCell.classList.add('unclaimed-cell');
+                }
+                newCell.dataset.xCoord = (j);
+                newCell.dataset.yCoord = (i);
+                this.boardContainer.appendChild(newCell);
             }
-            board.appendChild(newCell);
+        }
+    };
+    this.getCellByCoords = function(coords) {
+        return this.boardContainer.querySelector('#'+this.toCoordsString(coords));
+    };
+    this.getAllCells = function() {
+        return Array.from(this.boardContainer.querySelectorAll('.board-cell'));
+    };
+    this.setBoardStyle = function(columns, rows, cellSize) {
+        this.boardContainer.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
+        this.boardContainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`
+        this.boardContainer.style.width = cellSize * columns + 'px';
+        this.boardContainer.style.height = cellSize * rows + 'px';
+    };
+    this.drawBoard = function(columns, rows, cellSize=20) {
+        this.generateCells(columns, rows);
+        this.setBoardStyle(columns, rows, cellSize);
+    };
+    this.removeAllEntities = function() {
+        const currentEntities = this.boardContainer.querySelectorAll('.ball');
+        for (i = 0; i < currentEntities.length; i++) {
+            currentEntities[i].parentNode.removeChild(currentEntities[i]);
+        }
+    };
+    this.drawBall = function(coords, ballType) {
+        const cell = this.getCellByCoords(coords);
+        if (!cell.firstChild) {
+            // only display one ball per cell
+            const ball = document.createElement('div');
+            ball.classList.add('ball');
+            ball.classList.add(ballType);
+            cell.appendChild(ball);
         }
     }
-}
-
-function setBoardStyle(columns, rows, cellSize) {
-    board.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
-    board.style.gridTemplateRows = `repeat(${rows}, 1fr)`
-    board.style.width = cellSize * columns + 'px';
-    board.style.height = cellSize * rows + 'px';
-}
-
-function drawBoard(columns, rows, cellSize=20) {
-    generateCells(columns, rows);
-    setBoardStyle(columns, rows, cellSize);
-}
-
-// Game Functions
-function toCoordsString(coords) {
-    return `x${coords.x}y${coords.y}`
-}
-
-const directions = {
-    LEFT: 37,
-    UP: 38,
-    RIGHT: 39,
-    DOWN: 40,
+    this.drawBoard(columns, rows);
 }
 
 function Ball(coords, boundaries, ballType) {
@@ -134,7 +148,11 @@ function Ball(coords, boundaries, ballType) {
 }
 function Player(coords, boundaries) {
     Ball.call(this, coords, boundaries, 'player-ball');
-    this.allowedCells = ['board-cell', 'claimed-cell', 'unclaimed-cell', 'live-cell'];
+    this.allowedCells = ['claimed-cell', 'unclaimed-cell', 'live-cell'];
+    this.directions = {
+        LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40,
+        W: 87, A: 65, S: 83, D: 68
+    };
     this.getTargetCoords = function() {
         let nextX = this.coords.x + this.velocity.x;
         let nextY = this.coords.y + this.velocity.y;
@@ -149,24 +167,28 @@ function Player(coords, boundaries) {
     };
     this.input = function(keyCode) {
         switch (keyCode) {
-            case directions.LEFT:
+            case this.directions.LEFT: case this.directions.A:
                 this.velocity.x = -1;
                 this.velocity.y = 0;
                 break
-            case directions.UP:
+            case this.directions.UP: case this.directions.W:
                 this.velocity.x = 0;
                 this.velocity.y = -1;
                 break
-            case directions.RIGHT:
+            case this.directions.RIGHT: case this.directions.D:
                 this.velocity.x = 1;
                 this.velocity.y = 0;
                 break
-            case directions.DOWN:
+            case this.directions.DOWN: case this.directions.S:
                 this.velocity.x = 0;
                 this.velocity.y = 1;
                 break
         }
-    }
+    };
+    this.handleInput = function(e) {
+        this.input(e.keyCode);
+    };
+    document.addEventListener('keydown', (e) => this.handleInput(e));
 }
 function BlackEnemy(coords, boundaries) {
     Ball.call(this, coords, boundaries, 'black-ball');
@@ -190,111 +212,129 @@ function RedEnemy(coords, boundaries) {
     };
 }
 
-const game = {
-    board: document.querySelector('.game-board'),
-    entities: [],
-    addBall: function(ball) {
+function Level(board) {
+    this.board = board;
+    this.entities = [];
+    this.addBall = function(ball) {
         if (ball.ballType == 'player-ball') {
             this.playerBall = ball;
         }
         this.entities.push(ball);
-    },
-    updateEntities: function() {
+    };
+    this.updateEntities = function() {
         for (i in this.entities) {
             const ball = this.entities[i];
-            const targetCell = board.querySelector('#'+toCoordsString(ball.getTargetCoords()));
+            const targetCell = this.board.getCellByCoords(ball.getTargetCoords());
             if (ball.canEnterCell(targetCell)) {
                 ball.updateCoords();
             } else {
                 const bounceTargetCoords = ball.getBounceTargetCoords();
                 const adjacentCellCoords = ball.getAdjacentCellCoords();
                 const relevantCells = {
-                    bounceTargetX: board.querySelector('#'+toCoordsString(bounceTargetCoords.xTarget)),
-                    bounceTargetY: board.querySelector('#'+toCoordsString(bounceTargetCoords.yTarget)),
-                    adjacentX: board.querySelector('#'+toCoordsString(adjacentCellCoords.xAdjacent)),
-                    adjacentY: board.querySelector('#'+toCoordsString(adjacentCellCoords.yAdjacent))
+                    bounceTargetX: this.board.getCellByCoords(bounceTargetCoords.xTarget),
+                    bounceTargetY: this.board.getCellByCoords(bounceTargetCoords.yTarget),
+                    adjacentX: this.board.getCellByCoords(adjacentCellCoords.xAdjacent),
+                    adjacentY: this.board.getCellByCoords(adjacentCellCoords.yAdjacent)
                 }
                 ball.setBounceVelocity(relevantCells);
                 ball.updateCoords();
             }
         }
-    },
-    claimStartVelocity: {},
-    startClaim: function() {
+    };
+    this.claimStartVelocity = {};
+    this.isClaiming = false;
+    this.startClaim = function() {
         this.isClaiming = true;
         this.claimStartVelocity = {};
         this.claimStartVelocity.x = this.playerBall.velocity.x;
         this.claimStartVelocity.y = this.playerBall.velocity.y;
-    },
-    paint: function() {
-    },
-    endClaim: function() {
+    };
+    this.activateSurroundedCells = function() {
+        const boardCells = Array.from(this.board.getAllCells());
+        const claimedCells = [];
+        for (let i = 0; i < boardCells.length; i++) {
+            if (false) {
+                claimedCells.push(boardCells[i]);
+            }
+        }
+        for (let i = 0; i < claimedCells.length; i++) {
+            claimedCells[i].classList.replace('unclaimed-cell', 'live-cell')
+        }
+    };
+    this.completeClaim = function() {
         this.isClaiming = false;
-        const boardCells = this.board.querySelectorAll('.board-cell');
+        const boardCells = this.board.getAllCells();
         // call paint function if returning to claimed cells in different direction
         if (this.claimStartVelocity.x != this.playerBall.velocity.x ||
             this.claimStartVelocity.y != this.playerBall.velocity.y) {
-            this.paint();
+            this.activateSurroundedCells();
         }
         for (let i = 0; i < boardCells.length; i++) {
             boardCells[i].classList.replace('live-cell', 'claimed-cell');
         }
-    },
-    breakClaim: function() {
+    };
+    this.breakClaim = function() {
         this.isClaiming = false;
         this.playerBall.reset();
-        const boardCells = this.board.querySelectorAll('.board-cell');
+        const boardCells = this.board.getAllCells();
         for (let i = 0; i < boardCells.length; i++) {
             boardCells[i].classList.replace('live-cell', 'unclaimed-cell');
         }
-    },
-    updateBoard: function() {
+    };
+    this.endClaim = function() {
+        this.isClaiming = false;
+    };
+    this.updateBoard = function() {
         const redEnemies = this.entities.filter((ball) => ball.ballType == 'red-ball');
         for (let i = 0; i < redEnemies.length; i++) {
-            let enemyCoords = toCoordsString(redEnemies[i].coords);
-            let redEnemyCell = document.getElementById(enemyCoords);
+            let redEnemyCell = this.board.getCellByCoords(redEnemies[i].coords);
             if (redEnemyCell.classList.contains('live-cell')) {
                 this.breakClaim();
             }
         }
-        const coords = toCoordsString(this.playerBall.coords);
-        const playerCell = document.getElementById(coords);
+        const playerCell = this.board.getCellByCoords(this.playerBall.coords);
         playerCell.classList.replace('unclaimed-cell', 'live-cell');
         if (playerCell.classList.contains('live-cell') && !this.isClaiming) {
             this.startClaim();
         } else if (playerCell.classList.contains('claimed-cell') && this.isClaiming) {
-            this.endClaim();
+            this.completeClaim();
         }
-    },
-    renderBoard: function() {
-        const currentEntities = this.board.querySelectorAll('.ball');
-        for (i = 0; i < currentEntities.length; i++) {
-            currentEntities[i].parentNode.removeChild(currentEntities[i]);
-        }
+    };
+    this.renderBoard = function() {
+        this.board.removeAllEntities();
         for (i in this.entities) {
-            const coords = toCoordsString(this.entities[i].coords);
-            const cell = document.getElementById(coords);
-            if (!cell.firstChild) {
-                // only display one ball per cell
-                const ball = document.createElement('div');
-                ball.classList.add('ball');
-                ball.classList.add(this.entities[i].ballType);
-                cell.appendChild(ball);
-            }
+            this.board.drawBall(this.entities[i].coords, this.entities[i].ballType);
         }
-    },
-    update: function() {
+    };
+    this.update = function() {
         this.updateEntities();
         this.updateBoard();
         this.renderBoard();
+    };
+    this.play = function() {
+        this.update();
+        if (this.playing) {setTimeout(() => this.play(), this.tickLength)}
+    };
+    this.setSpeed = function(tickLength) {
+        this.tickLength = tickLength;
+    };
+    this.start = function(tickLength) {
+        if (!this.playing) {
+            this.playing = true;
+            this.setSpeed(tickLength);
+            this.play(this.tickLength);
+        }
+    };
+    this.stop = function() {
+        this.playing = false;
+    };
+}
+
+// Prevent movement keys from having default browser behavior
+window.addEventListener('keydown', function(e){
+    switch(e.keyCode){
+        case 37: case 39: case 38:  case 40: // Arrow keys
+        case 87: case 65: case 83:  case 68: // WASD keys
+            e.preventDefault();
     }
-}
-
-function handleInput(e) {
-    playerBall.input(e.keyCode);
-}
-
-function updateBoard(tickLength) {
-    game.update()
-    setTimeout(() => updateBoard(tickLength), tickLength);
-}
+});
